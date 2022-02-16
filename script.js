d51a33b652873e9397bb838e779de513e7603825
Bruno Rocha de Lima <91151280+brunorochadelima@users.noreply.github.com>
2022-02-16 08:41:51 -0300

    Atualização de cores da marca Visutec Crafts

--- script.js
+++ script.js
@@ -322,39 +322,6 @@ function visuteccrafts_cor4() {
 
 
 
-function visuteccrafts_cor5() {
-  /* Get the text field */
-  var copyText = document.getElementById("visuteccrafts_cor5");
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-  /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
-
-  /* Alert the copied text 
-  alert("Copied the text: " + copyText.value);*/
-}
-
-
-function visuteccrafts_cor6() {
-  /* Get the text field */
-  var copyText = document.getElementById("visuteccrafts_cor6");
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-  /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
-
-  /* Alert the copied text 
-  alert("Copied the text: " + copyText.value);*/
-}
-
-
-
 function visutec_cor1() {
   /* Get the text field */
   var copyText = document.getElementById("visutec_cor1");
